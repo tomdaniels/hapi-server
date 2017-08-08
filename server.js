7e3687a1ac03f23e7f71015f52ed8a1864863752
tomdaniels <thomsondaniels@hotmail.com>
2017-08-08 15:14:43 +1000

    sets up api data with fetch

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
 'use strict';
 
+require('isomorphic-fetch');
 const Hapi = require('hapi');
 
 // Create a server with a host and port
 const server = new Hapi.Server();
 server.connection({
     host: 'localhost',
-    port: 8000
+    port: 9000
 });
 
 // Add the route
@@ -16,6 +17,37 @@ server.route({
     handler: function (request, reply) {
 
         return reply('<h1>hello world</h1>');
+    }
+});
+
+function locationServicesData() {
+  fetch('https://maps.googleapis.com/maps/api/place/nearbysearch/json?location=-33.8670522,151.1957362&radius=500&types=food&name=harbour',
+  {
+    method: 'POST',
+    mode: 'cors',
+  }).then(function(response) {
+    console.log('worked');
+    console.log(response);
+    if (!response.ok) {
+      console.log('Network Error');
+      throw new Error(response.status);
+    }
+    return response.json();
+  }).then(function(data) {
+    console.log('Received Data');
+    console.log(data);
+  }).catch(function(error) {
+    console.error('Fetch Failed with error:');
+    console.error(error);
+  });
+};
+
+server.route({
+    method: 'GET',
+    path:'/location',
+    handler: function (request, reply) {
+        locationServicesData()
+        return reply('Fetch API');
     }
 });
 
